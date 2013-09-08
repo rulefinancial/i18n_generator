aee574506772f8bbd205e81a1bc2f32cee577cf2
Rob Young <robyoung26@gmail.com>
2013-09-08 16:51:54 +0100

    Fix whitespace/indentation

--- lib/i18n_generator.rb
+++ lib/i18n_generator.rb
@@ -4,12 +4,12 @@ require 'i18n_generator/label_writer'
 
 module I18nGenerator
   class << self
-		def process(source_spreadsheet, output_directory)
+    def process(source_spreadsheet, output_directory)
       excel_parser = I18nGenerator::ExcelParser.new(source_spreadsheet)
       label_writer = I18nGenerator::LabelWriter.new(output_directory)
 
       labels = excel_parser.parse
       label_writer.write_labels(labels, excel_parser.translation_languages)
     end
-	end
+  end
 end
